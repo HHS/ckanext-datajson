e71a295397afe2ea6729600d90efb7c40a14b80e
Joshua Tauberer <jt@occams.info>
2013-07-25 16:00:11 +0000

    normalize some invalid resource format strings

--- ckanext/datajson/parse_datajson.py
+++ ckanext/datajson/parse_datajson.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_datajson_entry(datajson, package, defaults):
 	package["title"] = datajson.get("title", defaults.get("Title"))
 	package["notes"] = datajson.get("description", defaults.get("Notes"))
@@ -45,14 +47,14 @@ def parse_datajson_entry(datajson, package, defaults):
 			if d.get(k, "").strip() != "":
 				r = {
 					"url": d[k],
-					"format": d.get("format", "Query Tool" if k == "webService" else "Unknown").upper(),
+					"format": normalize_format(d.get("format", "Query Tool" if k == "webService" else "Unknown")),
 				}
 				extra(r, "Language", d.get("language"))
 				extra(r, "Size", d.get("size"))
 				
 				# work-around for Socrata-style formats array
 				try:
-					r["format"] = d["formats"][0]["label"].upper()
+					r["format"] = normalize_format(d["formats"][0]["label"])
 				except:
 					pass
 				
@@ -64,3 +66,15 @@ def extra(package, key, value):
 	if not value: return
 	package.setdefault("extras", []).append({ "key": key, "value": value })
 	
+def normalize_format(format):
+	# Format should be a file extension. But sometimes Socrata outputs a MIME type.
+	format = format.lower()
+	m = re.match(r"((application|text)/(\S+))(; charset=.*)?", format)
+	if m:
+		if m.group(1) == "text/plain": return "Text"
+		if m.group(1) == "application/zip": return "ZIP"
+		if m.group(1) == "application/vnd.ms-excel": return "XLS"
+		if m.group(1) == "application/x-msaccess": return "Access"
+		return "Other"
+	if format == "text": return "Text"
+	return format.upper() # hope it's one of our formats by converting to upprecase
