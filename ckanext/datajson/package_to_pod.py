6b52f1ba621f5c494b3980a8711ec17407ce442f
dwcaraway <dave@fogmine.com>
2013-09-20 09:05:34 -0400

    Modified build_datajson to include USMetadata plugin fields, modified build_organizations to point to localhost

--- ckanext/datajson/package_to_pod.py
+++ ckanext/datajson/package_to_pod.py
@@ -1,47 +1,59 @@
 import collections
 
+import logging
+
+log = logging.getLogger(__name__)
+
 def make_datajson_entry(package):
+
+    extras = dict([(x['key'], x['value']) for x in package['extras']])
+    log.debug('package: %s', package)
+
     return collections.OrderedDict([
-        ("title", package["title"]),
-        ("description", package["notes"]),
-        ("keyword", [t["display_name"] for t in package["tags"]]),
-        ("modified", extra(package, "Date Updated")),
-        ("publisher", package["author"]),
-        ("organization", package["organization"]),
-        # person
-        # mbox
-        ("identifier", package["id"]),
-        ("accessLevel", "public"), #TODO get from metadata
-        ("dataDictionary", extra(package, "Data Dictionary")),
-        ("accessURL", get_primary_resource(package).get("url", None)),
-        ("webService", get_api_resource(package).get("url", None)),
-        ("format", get_primary_resource(package).get("format", None)),
-        ("license", extra(package, "License Agreement")),
-        ("spatial", extra(package, "Geographic Scope")),
-        ("temporal", build_temporal(package)),
-        ("issued", extra(package, "Date Released")),
-        # accrualPeriodicity (frequency of publishing, not the collection frequency)
-        # language
-        ("granularity", "/".join(x for x in [extra(package, "Unit of Analysis"), extra(package, "Geographic Granularity")] if x != None)),
-        ("dataQuality", True),
-        ("theme", [s for s in (extra(package, "Subject Area 1"), extra(package, "Subject Area 2"), extra(package, "Subject Area 3")) if s != None]),
-        ("references", [s for s in [extra(package, "Technical Documentation")] if s != None]),
-        # size
-        ("landingPage", package["url"]),
-        # feed
-        # systemOfRecords
-        ("distribution",
-            [
-                collections.OrderedDict([
-                	("identifier", r["id"]), # NOT in POD standard, but useful for conversion to JSON-LD
-                    ("accessURL" if r["format"].lower() not in ("api", "query tool") else "webService",
-                    	r["url"]),
-                    ("format", r["format"]),
-                    # language
-                    # size
-                ])
-                for r in package["resources"]
-            ]),
+        ("title", package.get("title", None)),
+        ("description", package.get("notes", None)),
+        ("keyword", [t["display_name"] for t in package.get("tags", None)]),
+        ("modified", package.get("metadata_modified", None)),
+        ("publisher", extras.get('publisher', package.get('author', None))),
+        ("organization", package.get("organization", None)),
+        ('contactName', extras.get('contact_name', None)),
+        ('contactEmail', extras.get('contact_email', None)),
+        ("identifier", extras.get('unique_id', package.get('id', None))),
+        ("accessLevel", extras.get('public_access_level', 'Public')),
+        ("dataDictionary", extras.get('data_dictionary', extras.get("Data Dictionary"))),
+        ("accessURL", get_primary_resource(package).get("url", None)), #TODO Is this common core?
+        ("webService", get_api_resource(package).get("url", None)), #TODO Is this common core?
+        ("format", get_primary_resource(package).get("format", None)), #TODO Is this common core?
+        ("license", extras.get("License Agreement", None)), #TODO grab license from package?
+        ('endpoint', extras.get('endpoint', None)), #TODO is this the same as the webService above?
+        ("spatial", extras.get('spatial', extras.get("Geographic Scope", None))),
+        ("temporal", extras.get('temporal', build_temporal(package))),
+        ("issued", extras.get('release_date', extras.get("Date Released", None))), #TODO should this be release date?
+        ('accrualPeriodicity', extras.get('accrual_periodicity', None)),
+        ('language', extras.get('language', None)),
+        ("granularity", extras.get('granularity', "/".join(x for x in [extras.get("Unit of Analysis", None), extras.get("Geographic Granularity", None)] if x != None))),
+        ("dataQuality", extras.get('data_quality', True)),
+        #        ("theme", [s for s in extras.get("Subject Area 1", None),extras.get("Subject Area 2", None), extras.get("Subject Area 3", None)) if s != None]),
+        ("references", [s for s in [extras.get("Technical Documentation", None)] if s != None]),
+        ('size', extras.get('size', None)),
+        ("landingPage", extras.get('homepage_url', package["url"])), #Is this the same as homepage url?
+         ('rssFeed', extras.get('rss_feed', None)),
+         ('category', extras.get('category', None)),
+         ('relatedDocuments', extras.get('related_documents', None)),
+         ('systemOfRecords', extras.get('system_of_records', None)),
+         ('systemOfRecordsNoneRelatedToThisDataset', extras.get('system_of_records_none_related_to_this_dataset', None)),
+         ("distribution",
+          [
+              collections.OrderedDict([
+                  ("identifier", r["id"]), # NOT in POD standard, but useful for conversion to JSON-LD
+                  ("accessURL" if r["format"].lower() not in ("api", "query tool") else "webService",
+                   r["url"]),
+                  ("format", r["format"]),
+                  # language
+                  # size
+              ])
+              for r in package["resources"]
+          ])
     ])
     
 def extra(package, key, default=None):
@@ -60,7 +72,7 @@ def get_best_resource(package, acceptable_formats):
 def get_primary_resource(package):
     # Return info about a "primary" resource. Select a good one.
     return get_best_resource(package, ("csv", "xls", "xml", "text", "zip", "rdf"))
-    
+
 def get_api_resource(package):
     # Return info about an API resource.
     return get_best_resource(package, ("api", "query tool"))
