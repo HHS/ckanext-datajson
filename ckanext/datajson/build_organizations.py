37fd387658a696ba0a215aa7e723ff06571d882c
dwcaraway <dave@fogmine.com>
2013-09-23 12:49:41 -0400

    Fixed bug that prevented org data.json files from being created, refactored the code, reduced to single main() function and added conditional main() invocation when running from commandline, added ability to supply the output dir, added fallback that when data.json found at localhost/data.json, that it will fallback to localhost:5000/data.json (useful when testing with paster vs live apache served content)

--- ckanext/datajson/build_organizations.py
+++ ckanext/datajson/build_organizations.py
@@ -1,27 +1,56 @@
 import simplejson as json
-import urllib2
+import urllib2, os, sys
 
-response = urllib2.urlopen('http://localhost/data.json')
-data = json.load(response)
+#Change these variables based on environment
+big_datajson_source = 'http://localhost/data.json'
+base_path = os.path.dirname(os.path.realpath(__file__))
+dest_folder = 'organizations'
+default_dest = os.path.join(base_path, dest_folder)
 
-# First we build a list of all of the organizations represented in the file
-def build_organization_list():
-	organizations = []
-	for dataset in data:
-		organizations.append(dataset['organization']['name'])
-	return list(set(organizations))
+def main(dest=None):
+    """
+    Build out the JSON file for each organization
+    """
+    output_dir = dest if dest else default_dest
 
-organizations = build_organization_list()
+    #Make sure the destination folder exists
+    if not dest and not os.path.exists(default_dest):
+        os.makedirs(default_dest)
 
-# We'll build out the JSON file for each organization
-def build_organization_json():
-	for organization in organizations:
-		org_json = []
-		for dataset in data:
-			if organization in dataset['organization']['name']:
-				org_name = dataset['organization']['name']
-				org_json.append(dataset)
-				with open('/usr/lib/ckan/datajson/ckanext/datajson/organizations/' + org_name + '.json', 'w') as outfile:
-					json.dump(org_json, outfile)
+    #Get the contents of the big data.json file
+    response = None
+    try:
+        response = urllib2.urlopen(big_datajson_source)
+    except urllib2.URLError:
+        #Fall back on default
+        response = urllib2.urlopen('http://localhost:5000/data.json')
 
-build_organization_json()
+    datasets = json.load(response)
+
+    #group datasets by organization
+    datasets_grouped_by_org = {}
+    for dataset in datasets:
+        org = dataset['organization']
+        if org in datasets_grouped_by_org:
+            datasets_grouped_by_org[org].append(dataset)
+        else:
+            datasets_grouped_by_org[org]=[dataset]
+
+    #write the organization.json files
+    for org_name, org_datasets in datasets_grouped_by_org.iteritems():
+        with open(os.path.join(output_dir,org_name + '.json'), 'w') as outfile:
+            json.dump(org_datasets, outfile)
+
+if __name__=="__main__":
+    if len(sys.argv)>2:
+        print "build_organizations.py <optional:/path/to/output/folder>"
+
+    if len(sys.argv)==2:
+        if not os.path.exists(sys.argv[1]):
+            print "Path does not exist: {0}".format(sys.argv[1])
+        elif not os.path.isdir(sys.argv[1]):
+            print "Not a directory: {0}".format(sys.argv[1])
+        else:
+            main(sys.argv[1])
+    else:
+        main()
