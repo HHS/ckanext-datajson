bd295edaa0d85ca95199ba10e33680867f7ac12e
Joshua Tauberer <jt@occams.info>
2013-12-09 00:29:10 +0000

    bump version signature to force a re-harvest of everything

--- ckanext/datajson/harvester_datajson.py
+++ ckanext/datajson/harvester_datajson.py
@@ -7,7 +7,7 @@ class DataJsonHarvester(DatasetHarvesterBase):
     A Harvester for /data.json files.
     '''
 
-    HARVESTER_VERSION = "0.9al" # increment to force an update even if nothing has changed
+    HARVESTER_VERSION = "0.9am" # increment to force an update even if nothing has changed
 
     def info(self):
         return {
