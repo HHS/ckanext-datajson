6e40cf59a6a7ad1a9671640bcd939ef1545e4715
Joshua Tauberer <jt@occams.info>
2014-01-07 14:41:17 +0000

    granularity is no longer in the POD schema since 1.0

--- ckanext/datajson/build_datajson.py
+++ ckanext/datajson/build_datajson.py
@@ -36,7 +36,6 @@ def make_datajson_entry(package, plugin):
         ("accrualPeriodicity", extra(package, "Publish Frequency")),
         ("language", extra(package, "Language")),
         ("PrimaryITInvestmentUII", extra(package, "PrimaryITInvestmentUII")),
-        ("granularity", "/".join(x for x in [extra(package, "Unit of Analysis"), extra(package, "Geographic Granularity")] if x != None)),
         ("dataQuality", extra(package, "Data Quality Met", default="true") == "true"),
         ("theme", [s for s in (extra(package, "Subject Area 1"), extra(package, "Subject Area 2"), extra(package, "Subject Area 3")) if s != None]),
         ("references", [s for s in extra(package, "Technical Documentation", default="").split(" ") if s != ""]),
