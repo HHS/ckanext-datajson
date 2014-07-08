ecb51baa9433a3ed1185b56a38eb19c10353835b
John Stange <john.stange@eglobaltech.com>
2014-07-08 19:48:52 +0000

    add data.jsonhhs feed of HHS-only datasets

--- ckanext/datajson/plugin.py
+++ ckanext/datajson/plugin.py
@@ -26,6 +26,7 @@ class DataJsonPlugin(p.SingletonPlugin):
     	# is called before after_map, in which we need the configuration directives
     	# to know how to set the paths.
         DataJsonPlugin.route_path = config.get("ckanext.datajson.path", "/data.json")
+        DataJsonPlugin.route_hhs_path = config.get("ckanext.datajsonhhs.path", re.sub(r"\.json$", ".jsonhhs", DataJsonPlugin.route_path))
         DataJsonPlugin.route_ld_path = config.get("ckanext.datajsonld.path", re.sub(r"\.json$", ".jsonld", DataJsonPlugin.route_path))
         DataJsonPlugin.ld_id = config.get("ckanext.datajsonld.id", config.get("ckan.site_url"))
         DataJsonPlugin.ld_title = config.get("ckan.site_title", "Catalog")
@@ -49,6 +50,7 @@ class DataJsonPlugin(p.SingletonPlugin):
         # /data.json and /data.jsonld (or other path as configured by user)
         m.connect('datajson', DataJsonPlugin.route_path, controller='ckanext.datajson.plugin:DataJsonController', action='generate_json')
         m.connect('datajsonld', DataJsonPlugin.route_ld_path, controller='ckanext.datajson.plugin:DataJsonController', action='generate_jsonld')
+        m.connect('datajsonhhs', DataJsonPlugin.route_hhs_path, controller='ckanext.datajson.plugin:DataJsonController', action='generate_jsonhhs')
         
         # /pod/validate
         m.connect('datajsonvalidator', "/pod/validate", controller='ckanext.datajson.plugin:DataJsonController', action='validator')
@@ -82,7 +84,10 @@ class DataJsonController(BaseController):
         del response.headers["Pragma"]
         
         # output
-        data = make_json()
+        if format == 'json-hhs':
+            data = make_json_hhs()
+        else:
+            data = make_json()
         
         if format == 'json-ld':
             # Convert this to JSON-LD.
@@ -105,8 +110,12 @@ class DataJsonController(BaseController):
             
         return p.toolkit.literal(json.dumps(data, indent=2))
 
+
     def generate_json(self):
         return self.generate_output('json')
+
+    def generate_jsonhhs(self):
+        return self.generate_output('json-hhs')
         
     def generate_jsonld(self):
         return self.generate_output('json-ld')
@@ -155,6 +164,10 @@ class DataJsonController(BaseController):
         c.catalog_data.sort(key = lambda x : x.get("modified"), reverse=True)
 
         return render('html_rendition.html')
+
+def make_json_hhs():
+    packages = p.toolkit.get_action("current_package_list_with_resources")(None, {})
+    return [make_datajson_entry(pkg, DataJsonPlugin) for pkg in packages if pkg["type"] == "dataset" and pkg["author"] in ["Administration for Children and Families", "Administration for Community Living", "Agency for Healthcare Research and Quality", "Centers for Disease Control and Prevention", "Centers for Medicare & Medicaid Services", "Department of Health & Human Services", "Health Resources and Services Administration", "Indian Health Service", "National Cancer Institute", "National Institute on Drug Abuse", "National Institutes of Health", "National Library of Medicine", "Substance Abuse & Mental Health Services Administration", "U.S. Food and Drug Administration"] ]
 
 def make_json():
     # Build the data.json file.
