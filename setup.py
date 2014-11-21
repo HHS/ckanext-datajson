ab43187797c68d60f70417c5a3937b109b749b13
jayhuggins <jay.huggins@nrel.gov>
2014-11-21 10:25:53 -0700

    Update setup.py
    The datajson plugin was causing errors for me when I used paster commands.  I updated the plugin paths in setup.py, which allowed me to remove the import statements in ckanext/datajson/__init__.py, and seems to have removed the requirement for the "import ckanext" hack in apache.wsgi.

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
 	entry_points=\
 	"""
         [ckan.plugins]
-	datajson=ckanext.datajson:DataJsonPlugin
-	datajson_harvest=ckanext.datajson:DataJsonHarvester
-	cmsdatanav_harvest=ckanext.datajson:CmsDataNavigatorHarvester
+	datajson=ckanext.datajson.plugin:DataJsonPlugin
+	datajson_harvest=ckanext.datajson.harvester_datajson:DataJsonHarvester
+	cmsdatanav_harvest=ckanext.datajson.harvester_cmsdatanavigator:CmsDataNavigatorHarvester
 	""",
 )
