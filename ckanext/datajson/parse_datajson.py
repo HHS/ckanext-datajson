fc8069bdd9790ee3813ccafce461959c519dd650
fxia <fxia@reisystems.com>
2015-04-29 13:25:39 -0400

    complete the license list

--- ckanext/datajson/parse_datajson.py
+++ ckanext/datajson/parse_datajson.py
@@ -29,14 +29,35 @@ def parse_datajson_entry(datajson, package, defaults, schema_version):
     'Other (Public Domain)':'other-pd',
     'UK Open Government Licence (OGL)':'uk-ogl',
 
+    # add more to complete the list 
     'U.S. Public Domain Works':'us-pd',
-    'http://www.usa.gov/publicdomain/label/1.0/':'us-pd',
+    'www.usa.gov/publicdomain/label/1.0':'us-pd',
+
+    # url (without protocol and trailing slash ) can also be used as key 
+    'creativecommons.org/licenses/by/4.0':'cc-by',
+    'creativecommons.org/licenses/by-sa/4.0':'cc-by-sa',
+    'creativecommons.org/publicdomain/zero/1.0':'cc-by-sa',
+    'creativecommons.org/licenses/by-nc/4.0':'cc-nc',
+    'www.gnu.org/copyleft/fdl.html':'gfdl',
+    'opendatacommons.org/licenses/by/1-0':'odc-by',
+    'opendatacommons.org/licenses/odbl':'odc-odbl',
+    'opendatacommons.org/licenses/pddl':'odc-pddl',
+    'project-open-data.cio.gov/unknown-license/#v1-legacy/other-at':'other-at',
+    'project-open-data.cio.gov/unknown-license/#v1-legacy/other-nc':'other-nc',
+    'project-open-data.cio.gov/unknown-license/#v1-legacy/other-closed':'other-closed',
+    'project-open-data.cio.gov/unknown-license/#v1-legacy/other-open':'other-open',
+    'creativecommons.org/publicdomain/mark/1.0/other-pd':'other-pd',
+    'www.nationalarchives.gov.uk/doc/open-government-licence/version/3':'uk-ogl'
   }
 
-  if not datajson.get("license", ""):
+  license = datajson.get("license")
+  if not license:
     package["license_id"] = licenses.get("License Not Specified", "");
-  elif licenses.get(datajson.get("license", ""), ""):
-    package["license_id"] = licenses.get(datajson.get("license", ""), "")
+  else
+    license = license.replace("http://","")
+    license = license.replace("https://","")
+    license = license.rstrip('/')
+    package["license_id"] = licenses.get(license, "")
 
   # 3. package["maintainer_email"]
   if package.get("maintainer_email"):
