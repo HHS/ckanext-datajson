177c9d8719964feed341541aae9a4d1009944be5
Alex Perfilov <alexandr.perfilov@reisystems.com>
2017-01-25 15:46:16 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ import sys, os
 version = '0.1'
 
 setup(
-	name='ckanext-datajson',
-	version=version,
-	description="CKAN extension to generate /data.json",
-	long_description="""\
+    name='ckanext-datajson',
+    version=version,
+    description="CKAN extension to generate /data.json",
+    long_description="""\
 	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='U.S. Department of Health & Human Services',
-	author_email='',
-	url='http://www.healthdata.gov',
-	license='Public Domain',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.datajson'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='U.S. Department of Health & Human Services',
+    author_email='',
+    url='http://www.healthdata.gov',
+    license='Public Domain',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.datajson'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points= \
+        """
         [ckan.plugins]
-    datajson=ckanext.datajson:DataJsonPlugin
-	datajson_harvest=ckanext.datajson:DataJsonHarvester
-	cmsdatanav_harvest=ckanext.datajson:CmsDataNavigatorHarvester
-	""",
+        datajson=ckanext.datajson.plugin:DataJsonPlugin
+        datajson_harvest=ckanext.datajson.plugin:DataJsonHarvester
+        cmsdatanav_harvest=ckanext.datajson.plugin:CmsDataNavigatorHarvester
+        """,
 )
