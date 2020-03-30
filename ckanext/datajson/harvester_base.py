84cbe657cf477a347060140c64424eca4bf94ba9
Fuhu Xia <fxia@reisystems.com>
2020-03-30 12:55:51 -0400

    check fields size so it does not go beyond solr string capacity

--- ckanext/datajson/harvester_base.py
+++ ckanext/datajson/harvester_base.py
@@ -27,6 +27,11 @@ VALIDATION_SCHEMA = [
                         ('non-federal', 'Project Open Data (Non-Federal)'),
                     ]
 
+# watch out for these keys that their string values might go beyond Solr capacity
+# https://github.com/GSA/datagov-deploy/issues/953
+SIZE_CHECK_KEYS = ['spatial']
+MAX_SIZE = 32766
+
 def validate_schema(schema):
     if schema not in [s[0] for s in VALIDATION_SCHEMA]:
         raise Invalid('Unknown validation schema: {0}'.format(schema))
@@ -390,6 +395,13 @@ class DatasetHarvesterBase(HarvesterBase):
 
         return elem + msg
 
+    def _size_check(self, key, value):
+        msg = ''
+        if key in SIZE_CHECK_KEYS and len(value) >= MAX_SIZE:
+            msg = 'Size of %s is too large. Maximum allowed size is %i.' % (key, MAX_SIZE)
+
+        return msg
+
     def import_stage(self, harvest_object):
         # The import stage actually creates the dataset.
         
@@ -607,6 +619,12 @@ class DatasetHarvesterBase(HarvesterBase):
         unmapped = []
 
         for key, value in dataset_processed.iteritems():
+
+            oversized_message = self._size_check(key, value)
+            if oversized_message:
+                self._save_object_error(oversized_message, harvest_object, 'Import')
+                return None
+
             if key in skip_processed:
                 continue
             new_key = mapping_processed.get(key)
