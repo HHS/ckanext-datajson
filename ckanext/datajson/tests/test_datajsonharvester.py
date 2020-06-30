f979b752d9149b8d2d6ca3802f60367b709da43f
Andres Vazquez <andres@data99.com.ar>
2020-06-30 14:03:02 -0300

    Add test for collection gather order. Clean Data.json samples

--- ckanext/datajson/tests/test_datajsonharvester.py
+++ ckanext/datajson/tests/test_datajsonharvester.py
@@ -42,50 +42,73 @@ class TestDataJSONHarvester(object):
         reset_db()
         harvest_model.setup()
 
-    def run_source(self, url):
+    def run_gather(self, url):
         source = HarvestSourceObj(url=url)
         job = HarvestJobObj(source=source)
 
-        harvester = DataJsonHarvester()
+        self.harvester = DataJsonHarvester()
 
         # gather stage
         log.info('GATHERING %s', url)
-        obj_ids = harvester.gather_stage(job)
+        obj_ids = self.harvester.gather_stage(job)
         log.info('job.gather_errors=%s', job.gather_errors)
         log.info('obj_ids=%s', obj_ids)
         if len(obj_ids) == 0:
             # nothing to see
             return
 
-        harvest_object = harvest_model.HarvestObject.get(obj_ids[0])
-        log.info('ho guid=%s', harvest_object.guid)
-        log.info('ho content=%s', harvest_object.content)
+        self.harvest_objects = []
+        for obj_id in obj_ids:
+            harvest_object = harvest_model.HarvestObject.get(obj_id)
+            log.info('ho guid=%s', harvest_object.guid)
+            log.info('ho content=%s', harvest_object.content)
+            self.harvest_objects.append(harvest_object)
 
+        return obj_ids
+
+    def run_fetch(self):
         # fetch stage
-        log.info('FETCHING %s', url)
-        result = harvester.fetch_stage(harvest_object)
 
-        log.info('ho errors=%s', harvest_object.errors)
-        log.info('result 1=%s', result)
+        for harvest_object in self.harvest_objects:
+            log.info('FETCHING %s' % harvest_object.id)
+            result = self.harvester.fetch_stage(harvest_object)
 
+            log.info('ho errors=%s', harvest_object.errors)
+            log.info('result 1=%s', result)
+            if len(harvest_object.errors) > 0:
+                self.errors = harvest_object.errors
+                
+    def run_import(self):
         # import stage
-        log.info('IMPORTING %s', url)
-        result = harvester.import_stage(harvest_object)
+        datasets = []
+        for harvest_object in self.harvest_objects:
+            log.info('IMPORTING %s' % harvest_object.id)
+            result = self.harvester.import_stage(harvest_object)
 
-        log.info('ho errors 2=%s', harvest_object.errors)
-        log.info('result 2=%s', result)
-        log.info('ho pkg id=%s', harvest_object.package_id)
-        dataset = model.Package.get(harvest_object.package_id)
-        if dataset:
-            log.info('dataset name=%s', dataset.name)
-        errors = harvest_object.errors
+            log.info('ho errors 2=%s', harvest_object.errors)
+            log.info('result 2=%s', result)
+            if len(harvest_object.errors) > 0:
+                self.errors = harvest_object.errors 
+                
+            log.info('ho pkg id=%s', harvest_object.package_id)
+            dataset = model.Package.get(harvest_object.package_id)
+            if dataset:
+                datasets.append(dataset)
+                log.info('dataset name=%s', dataset.name)
 
-        return harvest_object, result, dataset, errors
+        return datasets 
+
+    def run_source(self, url):
+        self.run_gather(url)
+        self.run_fetch()
+        datasets = self.run_import()
+
+        return datasets
 
     def test_datason_arm(self):
         url = 'http://127.0.0.1:%s/arm' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
-
+        datasets = self.run_source(url=url)
+        dataset = datasets[0]
         # assert_equal(first element on list
         expected_title = "NCEP GFS: vertical profiles of met quantities at standard pressures, at Barrow"
         assert_equal(dataset.title, expected_title)
@@ -95,22 +118,34 @@ class TestDataJSONHarvester(object):
     
     def test_datason_usda(self):
         url = 'http://127.0.0.1:%s/usda' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
-        expected_title = "Pesticide Data Program"
+        datasets = self.run_source(url=url)
+        dataset = datasets[0]
+        expected_title = "Department of Agriculture Congressional Logs for Fiscal Year 2014"
         assert_equal(dataset.title, expected_title)
         tags = [tag.name for tag in dataset.get_tags()]
-        assert_in("baby", tags)
         assert_equal(len(dataset.resources), 1)
+        assert_in(munge_title_to_name("Congressional Logs"), tags)     
 
     def test_datajson_collection(self):
+        """ harvest from a source with a parent in the second place
+            We expect the gather stage to re-order to the forst place """
         url = 'http://127.0.0.1:%s/collections' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
-        parent_title = "Employee Relations Roundtables"
-        assert_equal(dataset.title, parent_title)
+        obj_ids = self.run_gather(url=url)
 
+        identifiers = []
+        for obj_id in obj_ids:
+            harvest_object = harvest_model.HarvestObject.get(obj_id)
+            content = json.loads(harvest_object.content)
+            identifiers.append(content['identifier'])
+            
+        # We always expect the parent to be the first on the list
+        expected_obj_ids = ['OPM-ERround-0001', 'OPM-ERround-0001-AWOL', 'OPM-ERround-0001-Retire']
+        assert_equal(expected_obj_ids, identifiers)
+        
     def test_datajson_reserverd_word_as_title(self):
         url = 'http://127.0.0.1:%s/error-reserved-title' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
+        self.run_source(url=url)
+        errors = self.errors
         expected_error_stage = "Import"
         assert_equal(errors[0].stage, expected_error_stage)
         expected_error_message = "title: Search. That name cannot be used."
@@ -118,7 +153,8 @@ class TestDataJSONHarvester(object):
     
     def test_datajson_large_spatial(self):
         url = 'http://127.0.0.1:%s/error-large-spatial' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
+        self.run_source(url=url)
+        errors = self.errors
         expected_error_stage = "Import"
         assert_equal(errors[0].stage, expected_error_stage)
         expected_error_message = "spatial: Maximum allowed size is 32766. Actual size is 309643."
@@ -126,7 +162,8 @@ class TestDataJSONHarvester(object):
 
     def test_datajson_null_spatial(self):
         url = 'http://127.0.0.1:%s/null-spatial' % mock_datajson_source.PORT
-        harvest_object, result, dataset, errors = self.run_source(url=url)
+        datasets = self.run_source(url=url)
+        dataset = datasets[0]
         expected_title = "Sample Title NUll Spatial"
         assert_equal(dataset.title, expected_title)
    
