e4d48a5f25aae4077957dc1ee36b8216f7696057
Andres Vazquez <andres@data99.com.ar>
2020-10-19 09:47:45 -0300

    Add canonical script for CKAN 2.8

---
+++ bin/travis-build-2.8.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Updating script ..."
+
+wget https://raw.githubusercontent.com/GSA/catalog.data.gov/canonical_script/tools/ci-scripts/circleci-build-catalog-next.bash
+wget https://raw.githubusercontent.com/GSA/catalog.data.gov/canonical_script/tools/ci-scripts/test-catalog-next.ini
+
+sudo chmod +x circleci-build-catalog-next.bash
+source circleci-build-catalog-next.bash
+
+echo "TESTING ckanext-datajson"
+nosetests --ckan --with-pylons=test-catalog-next.ini ckanext/datajson
