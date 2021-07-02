f11517c9f3c9a6f0847ccbb34c835aba5c7c041a
Nicholas Kumia <nicholas.kumia@reisystems.com>
2021-07-02 12:18:28 -0400

    removed m.connects because itls not relevant to blueprints

--- ckanext/datajson/plugin.py
+++ ckanext/datajson/plugin.py
@@ -51,9 +51,6 @@ class DataJsonPlugin(p.SingletonPlugin):
             'datajson_inventory_links_enabled': self.datajson_inventory_links_enabled
         }
 
-    def before_map(self, m):
-        return m
-
     # def after_map(self, m):
     #     if DataJsonPlugin.route_enabled:
     #         # /data.json and /data.jsonld (or other path as configured by user)
@@ -86,30 +83,3 @@ class DataJsonPlugin(p.SingletonPlugin):
 
     def get_blueprint(self):
         return blueprint.datapusher
-
-        if DataJsonPlugin.route_enabled:
-            # /data.json and /data.jsonld (or other path as configured by user)
-            m.connect('datajson_export', DataJsonPlugin.route_path,
-                      controller='ckanext.datajson.plugin:DataJsonController', action='generate_json')
-            m.connect('organization_export', '/organization/{org_id}/data.json',
-                      controller='ckanext.datajson.plugin:DataJsonController', action='generate_org_json')
-            # TODO commenting out enterprise data inventory for right now
-            # m.connect('enterprisedatajson', DataJsonPlugin.route_edata_path,
-            # controller='ckanext.datajson.plugin:DataJsonController', action='generate_enterprise')
-
-            # m.connect('datajsonld', DataJsonPlugin.route_ld_path,
-            # controller='ckanext.datajson.plugin:DataJsonController', action='generate_jsonld')
-
-        if DataJsonPlugin.inventory_links_enabled:
-            m.connect('public_data_listing', '/organization/{org_id}/redacted.json',
-                      controller='ckanext.datajson.plugin:DataJsonController', action='generate_redacted')
-
-            m.connect('enterprise_data_inventory', '/organization/{org_id}/unredacted.json',
-                      controller='ckanext.datajson.plugin:DataJsonController', action='generate_unredacted')
-
-            m.connect('enterprise_data_inventory', '/organization/{org_id}/draft.json',
-                      controller='ckanext.datajson.plugin:DataJsonController', action='generate_draft')
-
-        # /pod/validate
-        m.connect('datajsonvalidator', "/pod/validate",
-                  controller='ckanext.datajson.plugin:DataJsonController', action='validator')
